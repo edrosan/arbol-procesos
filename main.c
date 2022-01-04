051586c7d08b9c276f5d1e2f87be8e730d031f14
Eduardo <edo.rod.san@gmail.com>
2022-01-04 01:22:53 -0600

    add

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+
+    int i=0, j, k;
+    int statusQ, statusP, statusR, statusI, statusM;
+    int statusA, statusV, statusE, statusr, statusa;
+
+    pid_t pid_Q, pid_P, pid_R, pid_I, pid_M;
+    pid_t pid_A, pid_V, pid_E, pid_r, pid_a;
+
+    if( (pid_Q = fork()) == 0){ // se crea Q
+        if((pid_I=fork()) == 0){// I
+            if((pid_P = fork()) == 0){
+                printf("P   %d   %d\n", getpid(), getppid());
+            }else{
+                if((pid_R = fork()) == 0){
+                    waitpid(pid_P, &statusP, 0);
+                    printf("R   %d   %d\n", getpid(), getppid());
+                }else{
+                    waitpid(pid_R, &statusR, 0);
+                    printf("I   %d  %d\n", getpid(), getppid());
+                }
+            }
+            
+        }
+        else{// Q
+            waitpid(pid_I, &statusI, 0);
+            if((pid_A = fork()) == 0){ // se crea A
+                if((pid_M = fork()) == 0){
+                    printf("M   %d   %d\n", getpid(), getppid());
+                }else{// A
+                    waitpid(pid_M, &statusM, 0);
+                    printf("A   %d   %d\n", getpid(), getppid());
+                }
+            }else{// Q
+                waitpid(pid_A, &statusA, 0);
+                if((pid_a = fork()) == 0){
+                    if((pid_V = fork()) == 0){// se crea V
+                        printf("V   %d   %d\n", getpid(), getppid());
+                    }else{
+                        waitpid(pid_V, &statusV, 0);
+                        if((pid_r = fork()) == 0){
+                            if((pid_E = fork()) == 0){
+                                printf("E   %d   %d\n", getpid(), getppid());
+                            }else{
+                                waitpid(pid_E, &statusE, 0);
+                                printf("r   %d   %d\n", getpid(), getppid());
+                            }
+                        }else{
+                            waitpid(pid_r, &statusr, 0);
+                            printf("a   %d   %d\n", getpid(), getppid());
+                        }
+                    }
+                }else{
+                    waitpid(pid_a, &statusa, 0);
+                    printf("@   %d   %d\n", getpid(), getppid());
+                }
+            }
+        }
+    }else{
+        printf("ID - PID - PPID\n");
+        printf("-----------------\n");
+        waitpid(pid_Q, &statusQ, 0);
+        printf("-----------------\n");
+        printf("Terminado\n");
+    }
+}
